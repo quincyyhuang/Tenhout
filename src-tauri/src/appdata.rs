8e6ecfd8696f394699f55f34c57366910d531019
Quincy Huang <quincyyhuang@gmail.com>
2025-03-19 21:33:30 -0400

    save window size on exit

--- src-tauri/src/appdata.rs
+++ src-tauri/src/appdata.rs
@@ -1,4 +1,24 @@
+use serde::{Deserialize, Serialize};
+
+pub const TENHOU_URLS: [&str; 3] = [
+    "https://tenhou.net/2/",
+    "https://tenhou.net/3/",
+    "https://tenhou.net/4/",
+];
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct AppData {
     pub window_width: u32,
     pub window_height: u32,
+    pub main_page_url: String,
+}
+
+impl AppData {
+    pub fn default() -> Self {
+        Self {
+            window_width: 800,
+            window_height: 600,
+            main_page_url: TENHOU_URLS[2].to_owned(),
+        }
+    }
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,80 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use appdata::AppData;
+use std::sync::Mutex;
+use tauri::Manager;
 mod appdata;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let mut app_data = appdata::AppData {
-        window_width: 800,
-        window_height: 600,
-    };
-
     tauri::Builder::default()
+        .setup(|app| {
+            // Try to read app settings from file
+            let app_config_dir = app
+                .path()
+                .app_config_dir()
+                .expect("Cannot get app config directory");
+
+            let app_config_path = app_config_dir.join("config.json");
+
+            let app_settings = match std::fs::read_to_string(&app_config_path) {
+                Ok(config_file) => match serde_json::from_str(&config_file) {
+                    Ok(data) => data,
+                    Err(_) => AppData::default(),
+                },
+                // Not config file found or error reading it
+                Err(_) => AppData::default(),
+            };
+
+            let url = tauri::Url::parse(&app_settings.main_page_url).unwrap();
+
+            // Handle window initialization
+            let webview_window =
+                tauri::WebviewWindowBuilder::new(app, "main", tauri::WebviewUrl::External(url))
+                    .build()?;
+
+            webview_window.set_size(tauri::LogicalSize {
+                width: app_settings.window_width.clone(),
+                height: app_settings.window_height.clone(),
+            })?;
+            webview_window.set_title("Tenhout")?;
+
+            // Set up app state management
+            let app_settings = Mutex::new(app_settings);
+            app.manage(app_settings);
+
+            // Handle window events
+            let app_handle = webview_window.app_handle().clone();
+            let scale_factor = webview_window.scale_factor().ok().unwrap();
+            webview_window.on_window_event(move |event| {
+                let app_settings = app_handle.state::<Mutex<AppData>>();
+                let mut app_settings = app_settings.lock().unwrap();
+
+                match event {
+                    // When window is resized, save the new window size
+                    tauri::WindowEvent::Resized(size) => {
+                        let logical_size: tauri::LogicalSize<u32> = size.to_logical(scale_factor);
+                        app_settings.window_width = logical_size.width;
+                        app_settings.window_height = logical_size.height;
+                    }
+                    // When window is closing, save the window size to config file
+                    tauri::WindowEvent::CloseRequested { api, .. } => {
+                        match serde_json::to_string(&*app_settings) {
+                            Ok(config) => {
+                                // Ensure config directory exists
+                                let _ = std::fs::create_dir_all(&app_config_dir);
+                                let _ = std::fs::write(&app_config_path.as_path(), &config);
+                            }
+                            Err(_) => {}
+                        };
+                    }
+                    _ => {}
+                }
+            });
+
+            // Show window
+            webview_window.show()?;
+            Ok(())
+        })
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_opener::init())
         .run(tauri::generate_context!())
